8f170a35778ebd324a3f70491922e2551567c936
Ilia <dotsenko.iv@edu.spbstu.ru>
2018-03-04 17:26:50 +0300

    Есть три фрейма Связанных Но не работающих еще как нужно

--- src/controllers/ControllerInformationFrame.java
+++ src/controllers/ControllerInformationFrame.java
@@ -1,11 +1,47 @@
 package controllers;
 
-public class ControllerInformationFrame {
-    public void toShow(){
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class ControllerInformationFrame {
+
+    public void toShow(ActionEvent actionEvent){
+        try {
+
+            final Node source = (Node) actionEvent.getSource();
+            final Stage stage1 = (Stage) source.getScene().getWindow();
+            stage1.close();
+
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("../fxml/information_frame_decription.fxml"));
+            stage.setResizable(true);
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void change(){
+    public void change(ActionEvent actionEvent){
+        try {
 
+            final Node source = (Node) actionEvent.getSource();
+            final Stage stage1 = (Stage) source.getScene().getWindow();
+            stage1.close();
+
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("../fxml/information_frame_change.fxml"));
+            stage.setResizable(true);
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
