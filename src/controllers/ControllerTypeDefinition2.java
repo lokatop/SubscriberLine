217e6dc022cf78293c323c0cd820ee6ad67c1270
elisevgen <eliseev2.ea@edu.spbstu.ru>
2018-03-30 14:23:52 +0300

    Определение типа аппаратных: - загрузка из InfoModels.xml - чекбоксы - поле ввода для количества

--- src/controllers/ControllerTypeDefinition2.java
+++ src/controllers/ControllerTypeDefinition2.java
@@ -1,14 +1,47 @@
 package controllers;
 
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.control.cell.TextFieldTreeTableCell;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
+import javafx.util.converter.IntegerStringConverter;
+import model.*;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControllerTypeDefinition2 {
+public class ControllerTypeDefinition2 implements Initializable {
     @FXML
     public VBox typeDefinition;
+
+    @FXML
+    private TableView _tableView;
+    @FXML
+    private TableColumn<TableViewApparatus, String> _tableColumn1;
+    @FXML
+    private TableColumn<TableViewApparatus, Integer> _tableColumn2;
+    @FXML
+    private TableColumn<TableViewApparatus, Boolean> _tableColumn3;
+    @FXML
+    private Label label;
+
+    //TODO Статическая переменная ниже передается и используется далее(чтобы изменения там сразу отражались и здесь)
+    public static ObservableList<InfoModel> apparatusData = FXCollections.observableArrayList();
+
 
     @FXML
     private void btnBackClick() throws IOException {
@@ -28,6 +61,83 @@ public class ControllerTypeDefinition2 {
         //typeDefinition.getChildren().setAll(vBox);
 
     }
-    @FXML
-    private void EditOriginalData(){}
+
+    public void readData(){
+        apparatusData.clear();
+
+        ObservableList unfilterred = FXCollections.observableArrayList();
+        unfilterred.addAll(XMLsaver.loadFromXML(InfoModel.FILENAME_INFOMODELS));
+
+        if (!unfilterred.isEmpty()) {
+
+            apparatusData.addAll(InfoModel.filterInfoModelByType("AOZU", unfilterred));
+            apparatusData.addAll(InfoModel.filterInfoModelByType("ATZU", unfilterred));
+        }
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        readData();
+
+        // Название
+        _tableColumn1.setCellValueFactory(new PropertyValueFactory<>("fullName"));
+
+        // Колчество
+        _tableColumn2.setCellValueFactory(new PropertyValueFactory<>("count"));
+        _tableColumn2.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        _tableColumn2.setMinWidth(10);
+        _tableColumn2.setOnEditCommit((TableColumn.CellEditEvent<TableViewApparatus, Integer> event) -> {
+            TablePosition<TableViewApparatus, Integer> pos = event.getTablePosition();
+
+            Integer newCount= event.getNewValue();
+
+            int row = pos.getRow();
+            TableViewApparatus apparatus = event.getTableView().getItems().get(row);
+
+            apparatus.setCount(newCount);
+        });
+
+        // Checkbox
+        _tableColumn3.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<TableViewApparatus, Boolean>, ObservableValue<Boolean>>() {
+            @Override
+            public ObservableValue<Boolean> call(TableColumn.CellDataFeatures<TableViewApparatus, Boolean> param) {
+                TableViewApparatus tableViewApparatus = param.getValue();
+
+                SimpleBooleanProperty booleanProperty = new SimpleBooleanProperty(tableViewApparatus.isChoose());
+
+                booleanProperty.addListener(new ChangeListener<Boolean>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                        tableViewApparatus.setChoose(newValue);
+                    }
+                });
+                return booleanProperty;
+            }
+        });
+        _tableColumn3.setCellFactory(new Callback<TableColumn<TableViewApparatus, Boolean>, TableCell<TableViewApparatus, Boolean>>() {
+            @Override
+            public TableCell<TableViewApparatus, Boolean> call(TableColumn<TableViewApparatus, Boolean> param) {
+                CheckBoxTableCell<TableViewApparatus, Boolean> cell = new CheckBoxTableCell<TableViewApparatus, Boolean>();
+                cell.setAlignment(Pos.CENTER);
+                return cell;
+            }
+        });
+
+        _tableView.setItems(getTableViewApparatusList());
+    }
+
+    private ObservableList<TableViewApparatus>  getTableViewApparatusList() {
+
+        //TableViewChooseCategory person1 = new TableViewChooseCategory("Susan Smith",  false);
+        //TableViewChooseCategory person2 = new TableViewChooseCategory("Anne McNeil",  false);
+        //TableViewChooseCategory person3 = new TableViewChooseCategory("Kenvin White", false);
+        ObservableList<TableViewApparatus> list = FXCollections.observableArrayList();
+
+        for (int i = 0; i<apparatusData.size(); i++){
+
+            list.add( new TableViewApparatus(
+                    apparatusData.get(i).getTitle()
+            ));
+        }
+        return list;
+    }
 }
---
+++ src/model/TableViewApparatus.java
@@ -0,0 +1,60 @@
+package model;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class TableViewApparatus {
+
+    private SimpleStringProperty fullName;
+    private SimpleIntegerProperty count;
+    private SimpleBooleanProperty choose;
+
+    public TableViewApparatus(String fullName, Integer count, boolean choose) {
+        this.fullName = new SimpleStringProperty(fullName);
+        this.count = new SimpleIntegerProperty(count);
+        this.choose = new SimpleBooleanProperty(choose);
+    }
+
+    public TableViewApparatus(String fullName) {
+        this.fullName = new SimpleStringProperty(fullName);
+        this.count = new SimpleIntegerProperty(1);
+        this.choose = new SimpleBooleanProperty(false);
+    }
+
+    public String getFullName() {
+        return fullName.get();
+    }
+
+    public SimpleStringProperty fullNameProperty() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName.set(fullName);
+    }
+
+    public boolean isChoose() {
+        return choose.get();
+    }
+
+    public SimpleBooleanProperty chooseProperty() {
+        return choose;
+    }
+
+    public void setChoose(boolean choose) {
+        this.choose.set(choose);
+    }
+
+    public int getCount() {
+        return count.get();
+    }
+
+    public SimpleIntegerProperty countProperty() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count.set(count);
+    }
+}
