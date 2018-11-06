a94e5377c258b299b5248e28dbd0b9548f8ce133
Ilia <dotsenko.iv@edu.spbstu.ru>
2018-11-06 23:34:43 +0300

    Добавляю специально недоделанный FirstTreadOnCalc Туда только запихнуть то что я скидывал в вк

--- src/thread/FirstTreadOnCalc_2.java
+++ src/thread/FirstTreadOnCalc_2.java
@@ -24,110 +24,51 @@ public class FirstTreadOnCalc_2 extends Thread {
             out = new FileOutputStream(new File("create_table.docx"));
 
 
-            // Create a new table with 7 rows and 6 columns
-            int nRows = observableList.size()+1;
-            int nCols = 5;
-            XWPFTable table = document.createTable(nRows, nCols);
+            XWPFParagraph paragraph = document.createParagraph();
+            XWPFRun run = paragraph.createRun();
 
-            // Get a list of the rows in the table
-            List<XWPFTableRow> rows = table.getRows();
-            int rowCt = 0;
-            int colCt = 0;
-            for (XWPFTableRow row : rows) {
-                // get table row properties (trPr)
-                CTTrPr trPr = row.getCtRow().addNewTrPr();
-                // set row height; units = twentieth of a point, 360 = 0.25"
-                CTHeight ht = trPr.addNewTrHeight();
-                ht.setVal(BigInteger.valueOf(360));
+            paragraph.setAlignment(ParagraphAlignment.RIGHT);
+            run.setFontFamily("Times New Roman");
+            run.setFontSize(14);
 
-                // get the cells in this row
-                List<XWPFTableCell> cells = row.getTableCells();
-                // add content to each cell
-                for (XWPFTableCell cell : cells) {
+            run.setText("Утверждаю   ");
+            run.addBreak();
+            run.setText("Звание__ Номер полка/роты и т.д.__ Полк/рота и т.д.   ");
+            run.addBreak();
+            run.setText("Должность__ И.Фамилия   ");
+            run.addBreak();
+            run.setText("Дата");
+            run.addBreak();
+            run.setText("");
+            run.addBreak();
+            run.setText("");
+            run.addBreak();
+            run.setText("");
+            run.addBreak();
 
+            XWPFParagraph paragraph2 = document.createParagraph();
+            XWPFRun run2 = paragraph2.createRun();
 
+            paragraph2.setAlignment(ParagraphAlignment.CENTER);
+            run2.setFontFamily("Times New Roman");
+            run2.setFontSize(14);
 
-                    // get a table cell properties element (tcPr)
-                    CTTcPr tcpr = cell.getCTTc().addNewTcPr();
-                    // set vertical alignment to "center"
-                    CTVerticalJc va = tcpr.addNewVAlign();
-                    va.setVal(STVerticalJc.CENTER);
+            run2.setText("РАСЧЕТ");
+            run2.addBreak();
+            run2.setText("развертывания абонентского оборудования на (где) (Номер полка/роты и т.д.__ Полк/рота и т.д.)");
+            run2.addBreak();
+            run2.setText("");
+            run2.addBreak();
 
-                    // create cell color element
-                    CTShd ctshd = tcpr.addNewShd();
-                    ctshd.setColor("auto");
-                    ctshd.setVal(STShd.CLEAR);
-                    if (rowCt == 0) {
-                        // header row
-                        ctshd.setFill("A7BFDE");
-                    } else if (rowCt % 2 == 0) {
-                        // even row
-                        ctshd.setFill("D3DFEE");
-                    } else {
-                        // odd row
-                        ctshd.setFill("EDF2F8");
-                    }
-
-                    // get 1st paragraph in cell's paragraph list
-                    XWPFParagraph para = cell.getParagraphs().get(0);
-                    // create a run to contain the content
-                    XWPFRun rh = para.createRun();
-
-
-                    if(rowCt != 0) {
-                        rh.setFontSize(10);
-                        rh.setFontFamily("Times New Roman");
-                        rh.setBold(false);
-                        para.setAlignment(ParagraphAlignment.CENTER);
-                        //----------------------------switch / case-----------
-                        switch (colCt) {
-                            case 0:
-                                rh.setText(observableList.get(rowCt-1).getOfficialPerson());
-                                break;
-                            case 1:
-                                rh.setText(observableList.get(rowCt-1).getTypeAbon());
-                                break;
-                            case 2:
-                                rh.setText(observableList.get(rowCt-1).getAppFrom1() + "       ");
-                                break;
-                            case 3:
-                                rh.setText(observableList.get(rowCt-1).getTypeCable() + "       ");
-                                break;
-                            case 4:
-                                rh.setText(" " + String.valueOf(observableList.get(rowCt-1).getLengthCable()) + " ");
-                                break;
-                        }
-                        //---------------------------The end of switch------------
-                    }else {
-
-                        rh.setFontSize(12);
-                        rh.setFontFamily("Times New Roman");
-                        rh.setBold(true);
-                        para.setAlignment(ParagraphAlignment.CENTER);
-
-                        switch (colCt) {
-                            case 0:
-                                rh.setText("Должностные лица");
-                                break;
-                            case 1:
-                                rh.setText("Тип абонентского устройства");
-                                break;
-                            case 2:
-                                rh.setText("Аппаратная");
-                                break;
-                            case 3:
-                                rh.setText("Тип кабеля");
-                                break;
-                            case 4:
-                                rh.setText("Длина кабеля");
-                                break;
-                        }}
-
-                    colCt++;
-                } // for cell
-                colCt = 0;
-                rowCt++;
-            }// for row
+            //!!!Вот тут первая и основная таблица создается
+            CreateTableMain(observableList,document);
+            //-----------------------------------------
+            CreateTableAbon(,document);
+            //-----------------------------------------
+            CreateTableCable(,document);
+            //-----------------------------------------
+            CreateTableTypeApp(,document);
+            //-----------------------------------------
 
             document.write(out);
             out.close();
@@ -137,6 +78,330 @@ public class FirstTreadOnCalc_2 extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+
+    public void CreateTableMain(ObservableList<TheLastTable> observableList, XWPFDocument document){
+
+        // Create a new table with 7 rows and 6 columns
+        int nRows = observableList.size()+1;
+        int nCols = 5;
+        XWPFTable table = document.createTable(nRows, nCols);
+
+        // Get a list of the rows in the table
+        List<XWPFTableRow> rows = table.getRows();
+        int rowCt = 0;
+        int colCt = 0;
+        for (XWPFTableRow row : rows) {
+            // get table row properties (trPr)
+            CTTrPr trPr = row.getCtRow().addNewTrPr();
+            // set row height; units = twentieth of a point, 360 = 0.25"
+            CTHeight ht = trPr.addNewTrHeight();
+            ht.setVal(BigInteger.valueOf(360));
+
+            // get the cells in this row
+            List<XWPFTableCell> cells = row.getTableCells();
+            // add content to each cell
+            for (XWPFTableCell cell : cells) {
+
+                // get a table cell properties element (tcPr)
+                CTTcPr tcpr = cell.getCTTc().addNewTcPr();
+                // set vertical alignment to "center"
+                CTVerticalJc va = tcpr.addNewVAlign();
+                va.setVal(STVerticalJc.CENTER);
+/*
+                    // create cell color element
+                    CTShd ctshd = tcpr.addNewShd();
+                    ctshd.setColor("auto");
+                    ctshd.setVal(STShd.CLEAR);
+                    if (rowCt == 0) {
+                        // header row
+                       // ctshd.setFill("A7BFDE");
+                    } else if (rowCt % 2 == 0) {
+                        // even row
+                        //ctshd.setFill("D3DFEE");
+                    } else {
+                        // odd row
+                        //ctshd.setFill("EDF2F8");
+                    }
+*/
+                // get 1st paragraph in cell's paragraph list
+                XWPFParagraph para = cell.getParagraphs().get(0);
+                // create a run to contain the content
+                XWPFRun rh = para.createRun();
+
+
+                if(rowCt != 0) {
+                    rh.setFontSize(11);
+                    rh.setFontFamily("Times New Roman");
+                    rh.setBold(false);
+                    para.setAlignment(ParagraphAlignment.CENTER);
+                    //----------------------------switch / case-----------
+                    switch (colCt) {
+                        case 0:
+                            rh.setText(observableList.get(rowCt-1).getOfficialPerson());
+                            break;
+                        case 1:
+                            rh.setText(observableList.get(rowCt-1).getTypeAbon());
+                            break;
+                        case 2:
+                            rh.setText(observableList.get(rowCt-1).getAppFrom1() + "       ");
+                            break;
+                        case 3:
+                            rh.setText(observableList.get(rowCt-1).getTypeCable() + "       ");
+                            break;
+                        case 4:
+                            rh.setText(" " + String.valueOf(observableList.get(rowCt-1).getLengthCable()) + " ");
+                            break;
+                    }
+                    //---------------------------The end of switch------------
+                }else {
+
+                    rh.setFontSize(11);
+                    rh.setFontFamily("Times New Roman");
+                    rh.setBold(false);
+                    para.setAlignment(ParagraphAlignment.CENTER);
+
+                    switch (colCt) {
+                        case 0:
+                            rh.setText("Должностные лица");
+                            break;
+                        case 1:
+                            rh.setText("Тип абонентского устройства");
+                            break;
+                        case 2:
+                            rh.setText("Аппаратная");
+                            break;
+                        case 3:
+                            rh.setText("Тип кабеля");
+                            break;
+                        case 4:
+                            rh.setText("Длина кабеля");
+                            break;
+                    }}
+
+                colCt++;
+            } // for cell
+            colCt = 0;
+            rowCt++;
+        }// for row
+
+    }
+    public void CreateTableAbon(, XWPFDocument document){
+
+        // Create a new table with n+1 rows and 2 columns
+        int nRows = observableList.size()+1;
+        int nCols = 2;
+        XWPFTable table2 = document.createTable(nRows, nCols);
+
+        // Get a list of the rows in the table
+        List<XWPFTableRow> rows = table2.getRows();
+        int rowCt = 0;
+        int colCt = 0;
+        for (XWPFTableRow row : rows) {
+            // get table row properties (trPr)
+            CTTrPr trPr = row.getCtRow().addNewTrPr();
+            // set row height; units = twentieth of a point, 360 = 0.25"
+            CTHeight ht = trPr.addNewTrHeight();
+            ht.setVal(BigInteger.valueOf(360));
+
+            // get the cells in this row
+            List<XWPFTableCell> cells = row.getTableCells();
+            // add content to each cell
+            for (XWPFTableCell cell : cells) {
+
+                // get a table cell properties element (tcPr)
+                CTTcPr tcpr = cell.getCTTc().addNewTcPr();
+                // set vertical alignment to "center"
+                CTVerticalJc va = tcpr.addNewVAlign();
+                va.setVal(STVerticalJc.CENTER);
+
+                // get 1st paragraph in cell's paragraph list
+                XWPFParagraph para = cell.getParagraphs().get(0);
+                // create a run to contain the content
+                XWPFRun rh = para.createRun();
+
+
+                if(rowCt != 0) {
+                    rh.setFontSize(11);
+                    rh.setFontFamily("Times New Roman");
+                    rh.setBold(false);
+                    para.setAlignment(ParagraphAlignment.CENTER);
+                    //----------------------------switch / case-----------
+                    switch (colCt) {
+                        case 0:
+                            rh.setText(observableList.get(rowCt-1).getOfficialPerson());
+                            break;
+                        case 1:
+                            rh.setText(observableList.get(rowCt-1).getTypeAbon());
+                            break;
+                    }
+                    //---------------------------The end of switch------------
+                }else {
+
+                    rh.setFontSize(11);
+                    rh.setFontFamily("Times New Roman");
+                    rh.setBold(true);
+                    para.setAlignment(ParagraphAlignment.CENTER);
+
+                    switch (colCt) {
+                        case 0:
+                            rh.setText("Тип абонентского оборудования");
+                            break;
+                        case 1:
+                            rh.setText("Количество");
+                            break;
+                    }}
+
+                colCt++;
+            } // for cell
+            colCt = 0;
+            rowCt++;
+        }// for row
+    }
+    public void CreateTableCable(, XWPFDocument document){
+
+        // Create a new table with n+1 rows and 2 columns
+        int nRows = observableList.size()+1;
+        int nCols = 2;
+        XWPFTable table2 = document.createTable(nRows, nCols);
+
+        // Get a list of the rows in the table
+        List<XWPFTableRow> rows = table2.getRows();
+        int rowCt = 0;
+        int colCt = 0;
+        for (XWPFTableRow row : rows) {
+            // get table row properties (trPr)
+            CTTrPr trPr = row.getCtRow().addNewTrPr();
+            // set row height; units = twentieth of a point, 360 = 0.25"
+            CTHeight ht = trPr.addNewTrHeight();
+            ht.setVal(BigInteger.valueOf(360));
+
+            // get the cells in this row
+            List<XWPFTableCell> cells = row.getTableCells();
+            // add content to each cell
+            for (XWPFTableCell cell : cells) {
+
+                // get a table cell properties element (tcPr)
+                CTTcPr tcpr = cell.getCTTc().addNewTcPr();
+                // set vertical alignment to "center"
+                CTVerticalJc va = tcpr.addNewVAlign();
+                va.setVal(STVerticalJc.CENTER);
+
+                // get 1st paragraph in cell's paragraph list
+                XWPFParagraph para = cell.getParagraphs().get(0);
+                // create a run to contain the content
+                XWPFRun rh = para.createRun();
+
+
+                if(rowCt != 0) {
+                    rh.setFontSize(11);
+                    rh.setFontFamily("Times New Roman");
+                    rh.setBold(false);
+                    para.setAlignment(ParagraphAlignment.CENTER);
+                    //----------------------------switch / case-----------
+                    switch (colCt) {
+                        case 0:
+                            rh.setText(observableList.get(rowCt-1).getOfficialPerson());
+                            break;
+                        case 1:
+                            rh.setText(observableList.get(rowCt-1).getTypeAbon());
+                            break;
+                    }
+                    //---------------------------The end of switch------------
+                }else {
+
+                    rh.setFontSize(11);
+                    rh.setFontFamily("Times New Roman");
+                    rh.setBold(true);
+                    para.setAlignment(ParagraphAlignment.CENTER);
+
+                    switch (colCt) {
+                        case 0:
+                            rh.setText("Тип кабеля");
+                            break;
+                        case 1:
+                            rh.setText("Длина");
+                            break;
+                    }}
+
+                colCt++;
+            } // for cell
+            colCt = 0;
+            rowCt++;
+        }// for row
+    }
+    public void CreateTableTypeApp(, XWPFDocument document){
+
+        // Create a new table with n+1 rows and 2 columns
+        int nRows = observableList.size()+1;
+        int nCols = 2;
+        XWPFTable table2 = document.createTable(nRows, nCols);
+
+        // Get a list of the rows in the table
+        List<XWPFTableRow> rows = table2.getRows();
+        int rowCt = 0;
+        int colCt = 0;
+        for (XWPFTableRow row : rows) {
+            // get table row properties (trPr)
+            CTTrPr trPr = row.getCtRow().addNewTrPr();
+            // set row height; units = twentieth of a point, 360 = 0.25"
+            CTHeight ht = trPr.addNewTrHeight();
+            ht.setVal(BigInteger.valueOf(360));
+
+            // get the cells in this row
+            List<XWPFTableCell> cells = row.getTableCells();
+            // add content to each cell
+            for (XWPFTableCell cell : cells) {
+
+                // get a table cell properties element (tcPr)
+                CTTcPr tcpr = cell.getCTTc().addNewTcPr();
+                // set vertical alignment to "center"
+                CTVerticalJc va = tcpr.addNewVAlign();
+                va.setVal(STVerticalJc.CENTER);
+
+                // get 1st paragraph in cell's paragraph list
+                XWPFParagraph para = cell.getParagraphs().get(0);
+                // create a run to contain the content
+                XWPFRun rh = para.createRun();
+
+
+                if(rowCt != 0) {
+                    rh.setFontSize(11);
+                    rh.setFontFamily("Times New Roman");
+                    rh.setBold(false);
+                    para.setAlignment(ParagraphAlignment.CENTER);
+                    //----------------------------switch / case-----------
+                    switch (colCt) {
+                        case 0:
+                            rh.setText(observableList.get(rowCt-1).getOfficialPerson());
+                            break;
+                        case 1:
+                            rh.setText(observableList.get(rowCt-1).getTypeAbon());
+                            break;
+                    }
+                    //---------------------------The end of switch------------
+                }else {
+
+                    rh.setFontSize(11);
+                    rh.setFontFamily("Times New Roman");
+                    rh.setBold(true);
+                    para.setAlignment(ParagraphAlignment.CENTER);
+
+                    switch (colCt) {
+                        case 0:
+                            rh.setText("Тип аппаратной");
+                            break;
+                        case 1:
+                            rh.setText("Количество");
+                            break;
+                    }}
+
+                colCt++;
+            } // for cell
+            colCt = 0;
+            rowCt++;
+        }// for row
     }
 
 }
